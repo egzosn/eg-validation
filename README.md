58c4b44422e92701c184292013e2eb3cf8bc1891
egzosn <egzosn@gmail.com>
2017-11-18 17:53:09 +0800

    校验器完成

--- README.md
+++ README.md
@@ -1,2 +1,146 @@
 # eg-validation
-表单数据校验
+## eg表单数据校验
+
+## 简单入门
+
+```html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Validation Form Template</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="jquery-1.11.3.min.js"></script>
+    <script src="eg-validation.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+
+<div class='well'>
+    <form class="form-horizontal" id="form" action="###">
+        <div >QQ:<input type="text" id="email" eg-valid="true" name="email"  eg-tips="请输入QQ号" eg-qq  eg-success="校验通过"  > </div>
+        <div >邮箱<input type="text"  eg-valid eg-email="正确的邮箱" eg-tips="请输入邮箱"  eg-required   eg-position="bottom"></div>
+        <button type="submit" class="btn">登录</button>
+    </form>
+</div>
+
+<script type="text/javascript" charset="utf-8">
+    $(function () {
+        $('#form').validation();
+    })
+</script>
+</body>
+</html>
+```
+
+
+#### 以下为字段基本属性的解释
+
+```
+    1. eg-valid                     是否开启校验 默认值：true
+    
+    2. eg-tips                      空文本域获取焦点后的提示消息
+    
+    3. eg-position                  校验结果展示位置 top、right、bottom、left  默认值：right
+    
+    4. eg-inline                    文本域所有的校验结果指定节点展示
+    
+    5. eg-{校验规则名}               开启对应的校验规则， 值为校验提示文本
+    
+    6. eg-{校验规则名}-param         校验规则对应的附加参数，非必须，根据校验规则参数而定，多个用","(逗号)隔开
+    
+    7. eg-{校验规则名}-inline        文本域指定的校验规则校验结果指定节点展示
+    
+    8. eg-success                    文本域校验通过后的提示信息  默认值： 校验通过
+    
+```
+    
+#### 以下几种默认的校验规则
+
+```
+
+    1. required                     必须输入的字段
+    
+    2. number                       必须输入合法的数字（负数，小数）
+    
+    3. digits                       必须输入整数
+    
+    4. decimal                      必须输入货币
+    
+    5. date                         必须输入正确格式的日期。日期校验 ie6 出错，慎用
+    
+    6. url                          必须输入正确格式的网址
+    
+    7. email                        必须输入正确格式的电子邮件
+    
+    8. char                         必须输入英文字符
+    
+    9. qq                           必须输入正确QQ
+    
+    10. phone                        必须输入正确手机号码
+    
+    11. equalto                      必须输入两次一至的字符，匹配文本域的唯一标识 ： eg-equalto-param=#password 
+    
+    12. chinese                      必须输入输入汉字
+    
+    13. minlength                    必须输入至少3位，eg-minlength-param=3
+    
+    14. maxlength                    必须输入至多6位，eg-minlength-param=6
+    
+    15. length                       必须输入长度必须介于 {3} 和 {6} 之间的字符串（汉字算一个字符）， eg-length-param=3,6
+    
+    16. min                          输入值不能小于3， eg-min-param=3
+    
+    17. max                          输入值不能大于6， eg-min-param=6
+    
+    18. pattern                      输入匹配对应的正则表达式， eg-pattern-param=/^\d+$/
+  
+      
+```
+
+
+#### 以下讲解该校验组件可使用的几个选项的属性(构造时传入的参数)
+
+```text
+
+    1. rules                        对象，校验规则集 
+    
+    2. isSubmit                     布尔型，校验通过后是否进行表单提交， 默认：true 进行表单提交
+    
+    2. valiAfter                    方法，所有表单域校验完成后回调方法
+
+```
+
+#### 下面对rules规则集的属性讲解
+
+**简单案例**
+```html
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Validation Form Template</title>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="jquery-1.11.3.min.js"></script>
+    <script src="eg-validation.js" type="text/javascript" charset="utf-8"></script>
+</head>
+<body>
+
+<div id="form">
+    <div >zip:<input type="text" eg-valid="true" eg-zip  eg-success="校验通过"> </div>
+ </div>
+
+<script type="text/javascript" charset="utf-8">
+    $(function () {
+        //这里增加邮编进行校验
+        $('#form').validation({
+               rules :{'zip':{validate: function(value) {return (/^[1-9][0-9]{5}$/.test(value) || /^.{4,15}$/.test(value));}, defaultMsg: '请输入正确邮编'}}
+        });
+    })
+</script>
+</body>
+</html>
+   
+```
+
+
+
--- eg-validation.js
+++ eg-validation.js
@@ -1,9 +1,7 @@
-
 !function($) {
     $.fn.validation = function(options) {
         $(document.head).append("<style>.error{border:1px solid #f00;padding:2px;}.error-inline{color:#ff5d5d;margin:05px;}.error-inline::before{content:'x';background-color:#ff5d5d;display:inline-block;width:14px;height:14px;line-height:10px;text-align:center;font-size:12px;border-radius:7px;overflow:hidden;color:#fff;margin-right:5px;}.warn{border:1px solid #ffdc2c;padding:2px;}.warn-inline{color:#ffdc2c;margin:05px;}.warn-inline::before{content:'!';background-color:#ffdc2c;display:inline-block;width:14px;height:14px;line-height:14px;text-align:center;font-size:12px;border-radius:7px;overflow:hidden;color:#fff;margin-right:5px;}.success{border:1px solid #11cd6d;padding:2px;}.success-inline{color:#11cd6d;margin:05px;}.success-inline::before{content:'√';background-color:#11cd6d;display:inline-block;width:14px;height:14px;line-height:14px;text-align:center;font-size:12px;border-radius:7px;overflow:hidden;color:#fff;margin-right:5px;}.tips{border: 1px solid #c6c6c6;padding:2px;}.tips-inline{color:#c6c6c6;margin:05px;}.tips-inline::before{content:'i';background-color:#c6c6c6;display:inline-block;width:14px;height:14px;text-align:center;font-size:12px;border-radius:7px;overflow:hidden;line-height:14px;color:#fff;margin-right:5px;}.inline-left{margin:05px;}.inline-right{margin:05px;position:absolute;}</style>");
         return this.each(function() {
-
             globalOptions = $.extend({}, $.fn.validation.defaults, options);
             validationForm(this)
         });
@@ -12,22 +10,31 @@
         //校验规则
         rules :{
             'required':{validate: function(value) {return ($.trim(value) != '');}, defaultMsg: '不能为空'},
-            'number':{validate: function(value) {return (/^[0-9]\d*$/.test(value));}, defaultMsg: '请输入数字。'},
+            'number':{validate: function(value) {return ( /^-?(?:\d+|\d{1,3}(?:,\d{3})+)?(?:\.\d+)?$/.test(value));}, defaultMsg: '请输入数字'},
+            'digits':{validate: function(value) {return (/^\d+$/.test(value));}, defaultMsg: '必须输入整数'},
+            'decimal':{validate: function(value) {return (/^\d{1,18}(.\d{1,2})?$/gi.test(value));}, defaultMsg: '请输入货币'},
+            'date':{validate: function(value) {return (!/Invalid|NaN/.test(new Date(value).toString()));}, defaultMsg: '请输入货币'},
+            'url':{validate: function(value) {return (/^(https?|s?ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value));}, defaultMsg: '请输入货币'},
             'email':{validate: function(value) {return (/^[a-zA-Z0-9]{1}([\._a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+){1,3}$/.test(value));}, defaultMsg: '请输入邮箱地址'},
             'char':{ validate: function(value) {return (/^[a-zA-Z]*$/.test(value));}, defaultMsg: '请输入英文字符'},
             'qq':{ validate: function(value) {return (/^[^0]\d{4,9}$/.test(value));}, defaultMsg: '请输入正确QQ'},
             'phone':{ validate: function(value) {return (/^((\(\d{2,3}\))|(\d{3}\-))?1(3|4|5|6|8)\d{9}$/.test(value));}, defaultMsg: '请输入正确手机号码'},
-            'password':{ validate: function(value) {return (safePassword(value));}, defaultMsg: '密码由字母和数字组成，至少6位'},
             'equalto':{ validate: function(value,param) { return ((value != $(param).val()));}, defaultMsg: '两次输入的字符不一至'},
-            'chinese':{ validate: function(value) {return (/^[\u4e00-\u9fff]$/.test(value));}, defaultMsg: '请输入汉字'},
-            'minlength':{ validate: function(value,param) {return (value.length>=param);}, defaultMsg: '请输入至少{param}位'},
-            'mlength':{ validate: function(value,param) {return (value.length<=param);}, defaultMsg: '请输入最多{param}位'},
+            'chinese':{ validate: function(value) {return (/^[\u4e00-\u9fff]+$/.test(value));}, defaultMsg: '请输入汉字'},
+            'minlength':{ validate: function(value,param) {return (value.length>=param);}, defaultMsg: '请输入至少{}位'},
+            'maxlength':{ validate: function(value,param) {return (value.length<=param);}, defaultMsg: '请输入至多{}位'},
+            'length':{ validate: function(value,param) {var p = param.split(",");return (value >= p[0] && value <= p[1]);}, defaultMsg: '输入长度必须介于 {} 和 {} 之间的字符串（汉字算一个字符）'},
+            'min':{ validate: function(value,param) {return (value<=param);}, defaultMsg: '输入值不能小于{}'},
+            'max':{ validate: function(value,param) {return (value>=param);}, defaultMsg: '输入值不能大于{}'},
             'pattern':{ validate: function(value,param) {return (new RegExp(param).test(value));}, defaultMsg: '请根据规则输入'},
 
         },
         //表单是否提交
-        isSubmit:true
-
+        isSubmit:true,
+        /**
+         *  校验完成后回调事件
+         */
+        valiAfter:function ($this) {return true}
     }
 
 
@@ -57,25 +64,59 @@
             el.before(dom);
         }
     };
-
     /**
      * 设置提示消息
      */
     var tips = function (el, type, msg, posName) {
         el.removeClass('error warn success tips');
-		
         el.addClass(type);
-		el.sib
         var offset = el.offset();
         var dom = $('<div/>');
         dom.text(msg);
         dom.addClass(type + "-inline");
         if (posName && pos[posName]){
-
             pos[posName](el, dom, offset);
         }else {
-            pos["bottom"](el, dom, offset);
+            pos["right"](el, dom, offset);
         }
+    }
+
+    /**
+     * 规则匹配
+     * @param el
+     * @param attr
+     * @param name
+     */
+    var matchRules = function (el, attr) {
+        var name = attr.name.replace("eg-", "");
+
+       var errorMsg = attr.value;
+        var param = el.attr(attr.name + "-param")
+        var rules = globalOptions.rules;
+        var rule = rules[name];
+        var  inline = el.attr(attr.name + "-inline") || el.attr("eg-inline");
+        var  line = null;
+        if (inline && (line=$(inline)).length != 0){
+            line.removeClass('error error-inline warn warn-inline success success-inline tips tips-inline');
+            line.text("");
+        }
+        if (null != rule && !rule.validate.call(el[0], el.val(),param)){
+            errorMsg = (!errorMsg)?rule.defaultMsg:errorMsg;
+            if (param){
+                var p = param.split(",");
+                for (var i in p){
+                    errorMsg = errorMsg.replace("{}", p);
+                }
+            }
+            if (inline && (line=$(inline)).length != 0){
+                line.addClass( "error-inline");
+                line.text(errorMsg);
+            }else {
+                tips(el, "error", errorMsg, el.attr("eg-position"))
+            }
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -84,41 +125,30 @@
      * @returns {boolean}
      */
     var validateField = function(field) { // 验证字段
-        var el = $(field), error = false, errorMsg = '';
+        var el = $(field), error = false;
 		
 	     el.siblings('[class$="-inline"]').remove();
         $.each(field.attributes,function (i, attr) {
             var name = attr.name;
-            var param = null;
-            if (!error && name　!= "eg-valid"  && name.indexOf("eg-") == 0 && name.indexOf("-inline") < 2 ){
-                name = name.replace("eg-", "");
-                errorMsg = attr.value;
-                param = el.attr(attr.name + "-param")
-                var rules = globalOptions.rules;
-                var rule = rules[name];
-				var  inline = el.attr(attr.name + "-inline") || el.attr("eg-inline");
-				var  line = null;
-				if (inline && (line=$(inline)).length != 0){
-					line.removeClass('error error-inline warn warn-inline success success-inline tips tips-inline');
-					line.text("");
-				}
-                if (null != rules[name] && !rule.validate.call(field, el.val(),param)){
-                    errorMsg = (!errorMsg)?rule.defaultMsg:errorMsg;
-                    error = true;
-                 	if (inline && (line=$(inline)).length != 0){
-                        line.addClass( "error-inline");
-                        line.text(errorMsg);
-                    }else {
-                        tips(el, "error", errorMsg, el.attr("eg-position"))
+
+            if (!error && name　!= "eg-valid"  &&  name.indexOf("eg-") == 0 && name.indexOf("-inline") < 2 ){
+                if (name != "eg-required" ){
+                    if (null != el.attr("eg-required") ){
+                        if (matchRules(el, {name:"eg-required", value: el.attr("eg-required")})){
+                            error =  true;
+                        }
+                    }else if (!field.value){
+                        error =  true;
                     }
+                }
+
+                if (!error && matchRules(el, attr)){
+                    error = true;
                 }
             }
         });
-        return error;
-
-
+        return  error;
     };
-
     var validation = function (el, that) {
         if("" == el.attr("eg-valid") || "true" ==  el.attr("eg-valid") ){
             if (validateField(that)) {
@@ -149,7 +179,6 @@
                                 inline += attr.value;
                             }
                         });
-
                         if("" == inline){
                             el.data("eg-inline", "!")
                         }else {
@@ -175,9 +204,6 @@
             });
         });
 
-
-
-
         $(obj).submit(function() { // 提交时验证
             if (formState) { // 重复提交则返回
                 return false;
@@ -190,12 +216,8 @@
                 }
             });
 
+            globalOptions.valiAfter(this);
             return globalOptions.isSubmit && !validationError;
-
         });
-
-
     };
-
-
 }(window.jQuery);
